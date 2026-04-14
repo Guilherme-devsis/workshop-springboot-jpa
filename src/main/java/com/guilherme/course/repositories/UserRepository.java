2964b6df8474b16b166c0a19454c2b861c8c0f1d
Guilherme <guilherme.640518@outlook.com>
2026-04-13 21:31:28 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/guilherme/course/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.guilherme.course.repositories;
+
+import com.guilherme.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
