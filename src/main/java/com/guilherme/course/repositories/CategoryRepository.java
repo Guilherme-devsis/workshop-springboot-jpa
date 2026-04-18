e9477bd671a1ab57e560783755bc1b652e37ce24
Guilherme <guilherme.640518@outlook.com>
2026-04-18 10:56:45 -0300

    Category

---
+++ src/main/java/com/guilherme/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.guilherme.course.repositories;
+
+import com.guilherme.course.entities.Category;
+import com.guilherme.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
