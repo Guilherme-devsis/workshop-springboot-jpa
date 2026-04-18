3ff6fbdc1350b558b06799606f21dbe441734b0b
Guilherme <guilherme.640518@outlook.com>
2026-04-18 13:56:19 -0300

    Product

--- src/main/java/com/guilherme/course/repositories/ProductRepository.java
+++ src/main/java/com/guilherme/course/repositories/ProductRepository.java
@@ -1,8 +1,9 @@
 package com.guilherme.course.repositories;
 
 import com.guilherme.course.entities.Category;
+import com.guilherme.course.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
